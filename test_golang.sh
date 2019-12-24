355b689474eac8f8afa89d72963c5015061bc155
Andrew Klotz <agc.klotz@gmail.com>
2019-12-24 22:02:13 +0000

    run golang a few times

--- test_golang.sh
+++ test_golang.sh
@@ -2,11 +2,14 @@
 
 set -x
 
-go test -count=1 -v 2>&1 ./golang/... | tee ./output.log
-go-junit-report < ./output.log > ./report.xml -set-exit-code
-test_status=$?
+for i in {0..5}; do
+  echo "$i"
+  go test -count=1 -v 2>&1 ./golang/... | tee ./output.log
+  go-junit-report < ./output.log > ./report.xml -set-exit-code
+  test_status=$?
+  ./testrecall-uploader -file report.xml
 
-./testrecall-uploader -file report.xml
-
-rm ./output.log
-exit "$test_status"
+  if [ $test_status -ne 0 ]; then
+    exit $test_status
+  fi
+done
