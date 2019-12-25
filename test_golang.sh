cc00ebd7bf83c6fc54d41a627275ba3900c80680
Andrew Klotz <agc.klotz@gmail.com>
2019-12-25 16:37:46 +0000

    build more data

--- test_golang.sh
+++ test_golang.sh
@@ -2,14 +2,15 @@
 
 set -x
 
+test_status=0
 for i in {0..10}; do
   echo "$i"
   go test -count=1 -v 2>&1 ./golang/... | tee ./output.log
   go-junit-report < ./output.log > ./report.xml -set-exit-code
   test_status=$?
   ./testrecall-uploader -file report.xml
-
-  if [ $test_status -ne 0 ]; then
-    exit $test_status
-  fi
 done
+
+if [ $test_status -ne 0 ]; then
+  exit $test_status
+fi
